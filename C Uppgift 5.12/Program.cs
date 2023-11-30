6ba6469cb891eef8a1e5dc3fd07ae690170156f9
HerrBergenholtz <zbdv8800@gmail.com>
2023-11-30 14:08:40 +0100

    Uppgift nästan färdig, felhantering behövs

--- C Uppgift 5.12/Program.cs
+++ C Uppgift 5.12/Program.cs
@@ -1,45 +1,91 @@
 ﻿using System;
 using System.Data.SqlTypes;
+using System.Collections.Generic;
 
 class Program
 {
     public static void Main()
     {
-        int[] tärning1 = { 1, 2, 3, 4, 5, 6 };
-        int[] tärning2 = { 1, 2, 3, 4, 5, 6 };
-        int[] tärning3 = { 1, 2, 3, 4, 5, 6 };
-        int[] tärning4 = { 1, 2, 3, 4, 5, 6 };
-        int[] tärning5 = { 1, 2, 3, 4, 5, 6 };
+        try
+        {
+            int[] tärningar = { 0, 0, 0, 0, 0};
 
-        int[][] tärningar = { tärning1, tärning2, tärning3, tärning4, tärning5 };
+            Console.WriteLine("Tärningarna kastas");
+            Console.ReadKey();
+            while (tärningar.Contains(0))
+            {
+                Tärningskast(tärningar);
+            }
 
-        Console.WriteLine("Tärningarna kastas");
-        Console.ReadKey();
-        Tärningskast(tärningar);
+            Console.WriteLine("Dina tärningar:");
+            Console.WriteLine();
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            foreach (int tärning in tärningar)
+            {
+                Console.Write($"|{tärning}|");
+            }
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.WriteLine();
+            Console.WriteLine("Tack för att du har spelat!");
+            Console.WriteLine("Vill du spela igen? (y/n)");
+            string svar = Console.ReadLine();
+
+            if (svar == "y")
+            {
+                Main();
+            }
+            else 
+            {
+                Environment.Exit(0);
+            }
+        }
+        catch
+        {
+            Console.WriteLine("Något gick fel, försök igen");
+            Main();
+        }
     }
 
-    public static void Tärningskast(int[][] tärningar)
+    public static void Tärningskast(int[] tärningar)
     {
-        int[] resultat = new int[5];
         int[] test = new int[5];
-        Random slump = new Random();
-        Console.WriteLine("1  2  3  4  5");
+        Random slump = new();
+        Console.WriteLine(" 1  2  3  4  5");
 
-        for (int i = 0; i < tärningar.Length; i++)
+        for (int i = 0; i < test.Length; i++)
         {
-            test[i] = tärningar[i][slump.Next(6)];
-            Console.Write($"|{test[i]}|");
+            test[i] = slump.Next(1, 7);
         }
+
+        for (int i = 0; i < test.Length; i++)
+        {
+            if (tärningar[i] != 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write($"|{tärningar[i]}|");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            else
+            {
+                Console.Write($"|{test[i]}|");
+            }
+        }
+        Console.WriteLine();
+
         Console.WriteLine("Skriv nummer på de tärningar du vill låsa in");
         string[] input = Console.ReadLine().Split(" ");
-        int[] inputInt = new int[6];
+
+        int[] inputInt = new int[input.Length];
         for (int i = 0;i < input.Length;i++)
         {
             inputInt[i] = int.Parse(input[i]);
         }
+
         for (int i = 0; i < input.Length; i++)
         {
-            resultat[i] = resultat.Concat(new int[] { inputInt[i]}).ToArray();
+            tärningar[inputInt[i] - 1] = test[inputInt[i] - 1];
         }
     }
 }
